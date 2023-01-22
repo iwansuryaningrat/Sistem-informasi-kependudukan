c2f7a252644a81bd4e524830844d122e0c6931fe
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-01-22 21:16:53 +0700

    Create All Controllers Needed

---
+++ app/Controllers/AdministrasiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class AdministrasiController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ app/Controllers/GaleriController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class GaleriController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
