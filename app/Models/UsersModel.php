faf21a7b7e51c59daa8a19f654ff07460b408717
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-01-22 21:03:56 +0700

    Create Users Model

--- app/Models/UsersModel.php
+++ app/Models/UsersModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class UserModel extends Model
+class UsersModel extends Model
 {
     protected $DBGroup          = 'default';
     protected $table            = 'users';
