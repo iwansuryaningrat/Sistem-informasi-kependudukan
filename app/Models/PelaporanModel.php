f435f3e4539d3cfe5af00a5f6501915fc32254bb
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-07 23:07:54 +0700

    Update Model Pelaporan

--- app/Models/PelaporanModel.php
+++ app/Models/PelaporanModel.php
@@ -6,37 +6,16 @@ use CodeIgniter\Model;
 
 class PelaporanModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'pelaporans';
-    protected $primaryKey       = 'id';
+    protected $table            = 'pelaporan';
+    protected $primaryKey       = 'pelaporan_id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['nama_pelaporan', 'nik', 'deskripsi_pelaporan', 'status_pelaporan', 'created_at', 'updated_at'];
 
     // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
