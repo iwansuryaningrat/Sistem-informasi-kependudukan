d6e1eab33cb796f3e7ef75702de4af3692eb1ab5
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-07 23:08:18 +0700

    Update Model Pengumuman

--- app/Models/PengumumanModel.php
+++ app/Models/PengumumanModel.php
@@ -6,37 +6,16 @@ use CodeIgniter\Model;
 
 class PengumumanModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'pengumumen';
-    protected $primaryKey       = 'id';
+    protected $table            = 'pengumuman';
+    protected $primaryKey       = 'pengumuman_id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['kategori', 'judul_pengumuman', 'deskripsi', 'tanggal', 'jam', 'tempat', 'status', 'created_by', 'created_at', 'updated_at'];
 
     // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
