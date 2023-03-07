15ebaf2a981476f09cc8e82f7592d5ecd1bef113
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-07 23:07:42 +0700

    Update Model Administrasi

--- app/Models/AdministrasiModel.php
+++ app/Models/AdministrasiModel.php
@@ -6,37 +6,16 @@ use CodeIgniter\Model;
 
 class AdministrasiModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'administrasis';
-    protected $primaryKey       = 'id';
+    protected $table            = 'administrasi';
+    protected $primaryKey       = 'administrasi_id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['pemohon', 'kategori', 'status', 'berkas', 'created_at', 'updated_at'];
 
     // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
