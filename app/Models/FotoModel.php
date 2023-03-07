45729945eb0ccd9849faea2d514e8c9f76ad615e
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-07 23:07:47 +0700

    Update Model Foto

--- app/Models/FotoModel.php
+++ app/Models/FotoModel.php
@@ -6,37 +6,16 @@ use CodeIgniter\Model;
 
 class FotoModel extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'fotos';
-    protected $primaryKey       = 'id';
+    protected $table            = 'foto';
+    protected $primaryKey       = 'foto_id';
     protected $useAutoIncrement = true;
-    protected $insertID         = 0;
     protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
     protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['galeri_id', 'nama_foto', 'foto_path', 'created_at', 'updated_at'];
 
     // Dates
     protected $useTimestamps = false;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
