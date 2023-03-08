ad2227c02482f1a90765a0e689cb4dcddde55480
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-08 17:44:15 +0700

    Update Footer

--- app/Views/Admin/template/footer.php
+++ app/Views/Admin/template/footer.php
@@ -4,24 +4,18 @@
         <nav class="pull-left">
             <ul class="nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="http://www.themekita.com">
-                        ThemeKita
-                    </a>
+
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        Help
-                    </a>
+
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        Licenses
-                    </a>
+
                 </li>
             </ul>
         </nav>
         <div class="copyright ml-auto">
-            2018, made with <i class="fa fa-heart heart text-danger"></i> by <a href="http://www.themekita.com">Iwan Suryaningrat</a>
+            Elsavira © 2023 Copyright. All right reserved
         </div>
     </div>
 </footer>
