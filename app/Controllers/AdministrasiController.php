c9ebfe427f6135fa1ca4e013d6bbd522cdde3cf2
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-14 00:37:07 +0700

    Create Methods that Used in Administrasi Controller

--- app/Controllers/AdministrasiController.php
+++ app/Controllers/AdministrasiController.php
@@ -2,10 +2,24 @@
 
 namespace App\Controllers;
 
+use App\Models\UsersModel;
+use App\Models\AdministrasiModel;
+
 use App\Controllers\BaseController;
 
 class AdministrasiController extends BaseController
 {
+
+    /* A variable that is used to store the object of the AdministrasiModel class. */
+    protected $administrasiModel;
+    protected $userModel;
+
+    public function __construct()
+    {
+        $this->administrasiModel = new AdministrasiModel();
+        $this->userModel = new UsersModel();
+    }
+
     public function index()
     {
         //
