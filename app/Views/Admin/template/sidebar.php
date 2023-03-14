99982f2123b109a9a2fe64de9cd43a211f4eb127
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-15 04:36:50 +0700

    Finishing Sidebar

--- app/Views/Admin/template/sidebar.php
+++ app/Views/Admin/template/sidebar.php
@@ -35,103 +35,115 @@
                 </div>
             </div>
             <ul class="nav nav-primary">
-                <li class="nav-item active">
+                <li class="nav-item <?php if ($active == 'dashboard') echo 'active' ?>">
                     <a data-toggle="collapse" href="#dashboard" class="collapsed" aria-expanded="false">
                         <i class="fas fa-home"></i>
                         <p>Dashboard</p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a data-toggle="collapse" href="#base">
+                <li class="nav-item <?php if ($active == 'penduduk') echo 'active' ?>">
+                    <a data-toggle="collapse" href="#penduduk">
                         <i class="fas fa-user-friends"></i>
-                        <p>Keluarga</p>
+                        <p>Penduduk</p>
                         <span class="caret"></span>
                     </a>
-                    <div class="collapse" id="base">
+                    <div class="collapse" id="penduduk">
                         <ul class="nav nav-collapse">
                             <li>
-                                <a href="components/avatars.html">
+                                <a href="/admin/families">
                                     <span class="sub-item">Daftar Keluarga</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="components/buttons.html">
+                                <a href="/admin/people">
                                     <span class="sub-item">Daftar Penduduk</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="components/gridsystem.html">
+                                <a href="/admin/addfamily">
                                     <span class="sub-item">Tambah Keluarga</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="components/panels.html">
+                                <a href="/admin/addpeople">
                                     <span class="sub-item">Tambah Penduduk</span>
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </li>
-                <li class="nav-item">
-                    <a data-toggle="collapse" href="#sidebarLayouts">
+                <li class="nav-item <?php if ($active == 'administrasi') echo 'active' ?>">
+                    <a data-toggle="collapse" href="/admin/administrasi">
                         <i class="fas fa-file-signature"></i>
                         <p>Administrasi</p>
-                        <span class="badge badge-success">4</span>
+                        <span class="badge badge-success">
+                            <?php if ($reqAdministrasi != 0 || $reqAdministrasi != null) {
+                                echo $reqAdministrasi;
+                            } else {
+                                echo 0;
+                            } ?>
+                        </span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a data-toggle="collapse" href="#forms">
+                <li class="nav-item <?php if ($active == 'pelaporan') echo 'active' ?>">
+                    <a data-toggle="collapse" href="/admin/pelaporan">
                         <i class="fas fa-pen-square"></i>
                         <p>Laporan</p>
-                        <span class="badge badge-success">4</span>
+                        <span class="badge badge-success">
+                            <?php if ($reqLaporan != 0 || $reqLaporan != null) {
+                                echo $reqLaporan;
+                            } else {
+                                echo 0;
+                            } ?>
+                        </span>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a data-toggle="collapse" href="#tables">
+                <li class="nav-item <?php if ($active == 'galeri') echo 'active' ?>">
+                    <a data-toggle="collapse" href="#galeri">
                         <i class="fas fa-images"></i>
                         <p>Galeri</p>
                         <span class="caret"></span>
                     </a>
-                    <div class="collapse" id="tables">
+                    <div class="collapse" id="galeri">
                         <ul class="nav nav-collapse">
                             <li>
-                                <a href="tables/tables.html">
+                                <a href="/admin/galeri">
                                     <span class="sub-item">Daftar Galeri</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="tables/datatables.html">
+                                <a href="/admin/foto">
                                     <span class="sub-item">Daftar Foto</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="tables/datatables.html">
+                                <a href="/admin/addgaleri">
                                     <span class="sub-item">Tambah Galeri</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="tables/datatables.html">
+                                <a href="/admin/addfoto">
                                     <span class="sub-item">Tambah Foto</span>
                                 </a>
                             </li>
                         </ul>
                     </div>
                 </li>
-                <li class="nav-item">
-                    <a data-toggle="collapse" href="#maps">
+                <li class="nav-item <?php if ($active == 'pengumuman') echo 'active' ?>">
+                    <a data-toggle="collapse" href="#pengumuman">
                         <i class="fas fa-bullhorn"></i>
                         <p>Pengumuman</p>
                         <span class="caret"></span>
                     </a>
-                    <div class="collapse" id="maps">
+                    <div class="collapse" id="pengumuman">
                         <ul class="nav nav-collapse">
                             <li>
-                                <a href="maps/googlemaps.html">
+                                <a href="/admin/pengumuman">
                                     <span class="sub-item">Riwayat Pengumuman</span>
                                 </a>
                             </li>
                             <li>
-                                <a href="maps/fullscreenmaps.html">
+                                <a href="/admin/addpengumuman">
                                     <span class="sub-item">Buat Pengumuman</span>
                                 </a>
                             </li>
