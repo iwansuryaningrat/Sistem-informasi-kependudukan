fed9cff823449f0a272adba8b7ccd0a6b1e98c11
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-14 16:49:55 +0700

    Update Home Controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,32 +2,21 @@
 
 namespace App\Controllers;
 
-use App\Controllers\AdministrasiController;
-use App\Controllers\AuthController;
 use App\Controllers\FotoController;
 use App\Controllers\GaleriController;
-use App\Controllers\KeluargaController;
-use App\Controllers\PelaporanController;
 use App\Controllers\PengumumanController;
 
 class Home extends BaseController
 {
-    protected $administrasiController;
-    protected $authController;
     protected $fotoController;
     protected $galeriController;
-    protected $keluargaController;
     protected $pelaporanController;
     protected $pengumumanController;
 
     public function __construct()
     {
-        $this->administrasiController = new AdministrasiController();
-        $this->authController = new AuthController();
         $this->fotoController = new FotoController();
         $this->galeriController = new GaleriController();
-        $this->keluargaController = new KeluargaController();
-        $this->pelaporanController = new PelaporanController();
         $this->pengumumanController = new PengumumanController();
     }
 
