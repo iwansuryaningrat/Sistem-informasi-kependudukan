b2e5540861717bea4b03c38b002868f7cfb55dcb
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-15 02:24:06 +0700

    Update Layout

--- app/Views/Admin/template/layout.php
+++ app/Views/Admin/template/layout.php
@@ -124,9 +124,6 @@
     <!-- Atlantis JS -->
     <script src="/assets/js/atlantis.min.js"></script>
 
-    <!-- Atlantis DEMO methods, don't include it in your project! -->
-    <script src="/assets/js/setting-demo.js"></script>
-    <script src="/assets/js/demo.js"></script>
     <?= $this->renderSection('script'); ?>
 </body>
 
