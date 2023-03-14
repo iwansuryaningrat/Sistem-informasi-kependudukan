a4b8d9a79e739ecb1adf3dd7f2d2cd38547afb1f
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-15 03:59:51 +0700

    Update Index.php

--- app/Views/Admin/index.php
+++ app/Views/Admin/index.php
@@ -31,7 +31,7 @@
 							</div>
 							<div class="px-2 pb-2 pb-md-0 text-center">
 								<div id="circles-3"></div>
-								<h6 class="fw-bold mt-3 mb-0">Subscribers</h6>
+								<h6 class="fw-bold mt-3 mb-0">xxx</h6>
 							</div>
 						</div>
 					</div>
@@ -39,18 +39,30 @@
 			</div>
 		</div>
 		<div class="row">
-			<div class="col-md-12">
+
+			<div class="col-md-6">
 				<div class="card">
 					<div class="card-header">
-						<div class="card-head-row">
-							<div class="card-title text-center">Persebaran Penduduk</div>
-						</div>
+						<div class="card-title">Persebaran Jenis Kelamin</div>
 					</div>
 					<div class="card-body">
-						<div class="chart-container" style="min-height: 375px">
-							<canvas id="statisticsChart"></canvas>
+						<div class="chart-container">
+							<canvas id="Gender" style="width: 50%; height: 50%"></canvas>
 						</div>
-						<div id="myChartLegend"></div>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="col-md-6">
+				<div class="card">
+					<div class="card-header">
+						<div class="card-title">Persebaran Usia</div>
+					</div>
+					<div class="card-body">
+						<div class="chart-container">
+							<canvas id="Usia" style="width: 50%; height: 50%"></canvas>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -173,66 +185,34 @@
 	});
 
 	//Chart
+	var Gender = document.getElementById('Gender').getContext('2d');
 
-	var ctx = document.getElementById("statisticsChart").getContext("2d");
-
-	var statisticsChart = new Chart(ctx, {
-		type: "line",
+	var Gender = new Chart(Gender, {
+		type: 'pie',
 		data: {
-			labels: [
-				"Jan",
-				"Feb",
-				"Mar",
-				"Apr",
-				"May",
-				"Jun",
-				"Jul",
-				"Aug",
-				"Sep",
-				"Oct",
-				"Nov",
-				"Dec",
-			],
 			datasets: [{
-					label: "Subscribers",
-					borderColor: "#f3545d",
-					pointBackgroundColor: "rgba(243, 84, 93, 0.6)",
-					pointRadius: 0,
-					backgroundColor: "rgba(243, 84, 93, 0.4)",
-					legendColor: "#f3545d",
-					fill: true,
-					borderWidth: 2,
-					data: [154, 184, 175, 203, 210, 231, 240, 278, 252, 312, 320, 374],
-				},
-				{
-					label: "New Visitors",
-					borderColor: "#fdaf4b",
-					pointBackgroundColor: "rgba(253, 175, 75, 0.6)",
-					pointRadius: 0,
-					backgroundColor: "rgba(253, 175, 75, 0.4)",
-					legendColor: "#fdaf4b",
-					fill: true,
-					borderWidth: 2,
-					data: [256, 230, 245, 287, 240, 250, 230, 295, 331, 431, 456, 521],
-				},
-				{
-					label: "Active Users",
-					borderColor: "#177dff",
-					pointBackgroundColor: "rgba(23, 125, 255, 0.6)",
-					pointRadius: 0,
-					backgroundColor: "rgba(23, 125, 255, 0.4)",
-					legendColor: "#177dff",
-					fill: true,
-					borderWidth: 2,
-					data: [542, 480, 430, 550, 530, 453, 380, 434, 568, 610, 700, 900],
-				},
-			],
+				data: [50, 35],
+				backgroundColor: ["#1d7af3", "#f3545d", "#fdaf4b"],
+				borderWidth: 0
+			}],
+			labels: ['Laki - Laki', 'Perempuan']
 		},
 		options: {
 			responsive: true,
 			maintainAspectRatio: false,
 			legend: {
-				display: false,
+				position: 'bottom',
+				labels: {
+					fontColor: 'rgb(154, 154, 154)',
+					fontSize: 11,
+					usePointStyle: true,
+					padding: 20
+				}
+			},
+			pieceLabel: {
+				render: 'percentage',
+				fontColor: 'white',
+				fontSize: 14,
 			},
 			tooltips: {
 				bodySpacing: 4,
@@ -241,69 +221,67 @@
 				position: "nearest",
 				xPadding: 10,
 				yPadding: 10,
-				caretPadding: 10,
+				caretPadding: 10
 			},
 			layout: {
 				padding: {
-					left: 5,
-					right: 5,
-					top: 15,
-					bottom: 15
-				},
-			},
-			scales: {
-				yAxes: [{
-					ticks: {
-						fontStyle: "500",
-						beginAtZero: false,
-						maxTicksLimit: 5,
-						padding: 10,
-					},
-					gridLines: {
-						drawTicks: false,
-						display: false,
-					},
-				}, ],
-				xAxes: [{
-					gridLines: {
-						zeroLineColor: "transparent",
-					},
-					ticks: {
-						padding: 10,
-						fontStyle: "500",
-					},
-				}, ],
-			},
-			legendCallback: function(chart) {
-				var text = [];
-				text.push('<ul class="' + chart.id + '-legend html-legend">');
-				for (var i = 0; i < chart.data.datasets.length; i++) {
-					text.push(
-						'<li><span style="background-color:' +
-						chart.data.datasets[i].legendColor +
-						'"></span>'
-					);
-					if (chart.data.datasets[i].label) {
-						text.push(chart.data.datasets[i].label);
-					}
-					text.push("</li>");
+					left: 20,
+					right: 20,
+					top: 20,
+					bottom: 20
 				}
-				text.push("</ul>");
-				return text.join("");
-			},
+			}
+		}
+	})
+
+	var Usia = document.getElementById('Usia').getContext('2d');
+
+	var Usia = new Chart(Usia, {
+		type: 'pie',
+		data: {
+			datasets: [{
+				data: [100, 35, 15, 33],
+				backgroundColor: ["#1d7af3", "#f3545d", "#fdaf4b", "#3CC47C"],
+				borderWidth: 0
+			}],
+			labels: ['<17', '17-25', '25-35', '>35']
 		},
-	});
-
-	var myLegendContainer = document.getElementById("myChartLegend");
-
-	// generate HTML legend
-	myLegendContainer.innerHTML = statisticsChart.generateLegend();
-
-	// bind onClick event to all LI-tags of the legend
-	var legendItems = myLegendContainer.getElementsByTagName("li");
-	for (var i = 0; i < legendItems.length; i += 1) {
-		legendItems[i].addEventListener("click", legendClickCallback, false);
-	}
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+			legend: {
+				position: 'bottom',
+				labels: {
+					fontColor: 'rgb(154, 154, 154)',
+					fontSize: 11,
+					usePointStyle: true,
+					padding: 20
+				}
+			},
+			pieceLabel: {
+				render: 'percentage',
+				fontColor: 'white',
+				fontSize: 14,
+			},
+			tooltips: {
+				bodySpacing: 4,
+				mode: "nearest",
+				intersect: 0,
+				position: "nearest",
+				xPadding: 10,
+				yPadding: 10,
+				caretPadding: 10
+			},
+			layout: {
+				padding: {
+					left: 20,
+					right: 20,
+					top: 20,
+					bottom: 20
+				}
+			}
+		}
+	})
 </script>
 
 <?= $this->endSection(); ?>
