15acb98fcf85157a6c7e6f80fa31ec619644774c
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-14 23:20:43 +0700

    Create CRUD Method at Foto Model

--- app/Models/FotoModel.php
+++ app/Models/FotoModel.php
@@ -18,4 +18,46 @@ class FotoModel extends Model
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
+
+    // Get Foto data join with galeri
+    public function getFoto($foto_id = null)
+    {
+        if ($foto_id == null) {
+            return $this->select('foto.*, galeri.nama_galeri')
+                ->join('galeri', 'galeri.galeri_id = foto.galeri_id')
+                ->findAll();
+        }
+
+        return $this->select('foto.*, galeri.nama_galeri')
+            ->join('galeri', 'galeri.galeri_id = foto.galeri_id')
+            ->where(['foto_id' => $foto_id])
+            ->first();
+    }
+
+    // Get Foto data by galeri_id
+    public function getFotoByGaleriId($galeri_id)
+    {
+        return $this->select('foto.*, galeri.nama_galeri')
+            ->join('galeri', 'galeri.galeri_id = foto.galeri_id')
+            ->where(['galeri_id' => $galeri_id])
+            ->findAll();
+    }
+
+    // Edit Foto data
+    public function editFoto($data, $foto_id)
+    {
+        return $this->db->table($this->table)->update($data, ['foto_id' => $foto_id]);
+    }
+
+    // Delete Foto data
+    public function deleteFoto($foto_id)
+    {
+        return $this->db->table($this->table)->delete(['foto_id' => $foto_id]);
+    }
+
+    // Save Foto data
+    public function saveFoto($data)
+    {
+        return $this->db->table($this->table)->insert($data);
+    }
 }
