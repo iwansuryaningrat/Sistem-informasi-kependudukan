21f1d7bc6c73585c368f6021f77304096ffa0c05
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-14 23:24:46 +0700

    Create CRUD Method at Pelaporan Model

--- app/Models/PelaporanModel.php
+++ app/Models/PelaporanModel.php
@@ -18,4 +18,49 @@ class PelaporanModel extends Model
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
+
+    // Get Pelaporan data join with user
+    public function getPelaporan($pelaporan_id = null)
+    {
+        if ($pelaporan_id == null) {
+            return $this->select('pelaporan.*, users.nama')
+                ->join('users', 'users.user_id = pelaporan.nik_pelapor')
+                ->join('users', "users.user_id = pelaporan.nik_terlapor")
+                ->findAll();
+        }
+
+        return $this->select('pelaporan.*, users.nama')
+            ->join('users', 'users.user_id = pelaporan.nik_pelapor')
+            ->join('users', "users.user_id = pelaporan.nik_terlapor")
+            ->where(['pelaporan_id' => $pelaporan_id])
+            ->first();
+    }
+
+    // Get Pelaporan data join with user by nik
+    public function getPelaporanByNik($nik)
+    {
+        return $this->select('pelaporan.*, users.nama')
+            ->join('users', 'users.user_id = pelaporan.nik_pelapor')
+            ->join('users', "users.user_id = pelaporan.nik_terlapor")
+            ->where(['nik_pelapor' => $nik])
+            ->findAll();
+    }
+
+    // Edit Pelaporan data
+    public function editPelaporan($data, $pelaporan_id)
+    {
+        return $this->db->table($this->table)->update($data, ['pelaporan_id' => $pelaporan_id]);
+    }
+
+    // Delete Pelaporan data
+    public function deletePelaporan($pelaporan_id)
+    {
+        return $this->db->table($this->table)->delete(['pelaporan_id' => $pelaporan_id]);
+    }
+
+    // Save Pelaporan data
+    public function savePelaporan($data)
+    {
+        return $this->db->table($this->table)->insert($data);
+    }
 }
