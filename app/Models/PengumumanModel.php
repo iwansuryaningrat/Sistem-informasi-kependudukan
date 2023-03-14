ac30fd5c1c387e746e35d40a34ad415d178a572b
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-15 01:45:49 +0700

    Add Sorting to Pengumuman Model

--- app/Models/PengumumanModel.php
+++ app/Models/PengumumanModel.php
@@ -19,12 +19,13 @@ class PengumumanModel extends Model
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
 
-    // Get Pengumuman data join with user
+    // Get Pengumuman data join with user and sort by created_at desc
     public function getPengumuman($pengumuman_id = null)
     {
         if ($pengumuman_id == null) {
             return $this->select('pengumuman.*, users.nama')
                 ->join('users', 'users.user_id = pengumuman.created_by')
+                ->orderBy('pengumuman.created_at', 'DESC')
                 ->findAll();
         }
 
@@ -40,6 +41,7 @@ class PengumumanModel extends Model
         return $this->select('pengumuman.*, users.nama')
             ->join('users', 'users.user_id = pengumuman.created_by')
             ->where(['kategori' => $kategori])
+            ->orderBy('pengumuman.created_at', 'DESC')
             ->findAll();
     }
 
