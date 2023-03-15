ee04fad5f933ac15286f1daa2ad517178e7ccd47
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-16 00:28:19 +0700

    Update Login Page

--- app/Views/Auth/login.php
+++ app/Views/Auth/login.php
@@ -3,7 +3,7 @@
 
 <head>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<title>Login</title>
+	<title><?= $title; ?></title>
 	<meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
 	<link rel="icon" href="/assets/img/icon.ico" type="image/x-icon" />
 
@@ -34,70 +34,28 @@
 		<div class="container container-login animated fadeIn">
 			<h3 class="text-center">Sign In To Admin</h3>
 			<div class="login-form">
-				<div class="form-group form-floating-label">
-					<input id="username" name="username" type="text" class="form-control input-border-bottom" required>
-					<label for="username" class="placeholder">Username</label>
-				</div>
-				<div class="form-group form-floating-label">
-					<input id="password" name="password" type="password" class="form-control input-border-bottom" required>
-					<label for="password" class="placeholder">Password</label>
-					<div class="show-password">
-						<i class="icon-eye"></i>
+				<form action="/auth/logingin" method="post">
+					<div class="form-group form-floating-label">
+						<input id="nik" name="nik" type="text" class="form-control input-border-bottom" required>
+						<label for="nik" class="placeholder">NIK</label>
 					</div>
-				</div>
-				<div class="row form-sub m-0">
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" id="rememberme">
-						<label class="custom-control-label" for="rememberme">Remember Me</label>
+					<div class="form-group form-floating-label">
+						<input id="password" name="password" type="password" class="form-control input-border-bottom" required>
+						<label for="password" class="placeholder">Password</label>
+						<div class="show-password">
+							<i class="icon-eye"></i>
+						</div>
 					</div>
-
-					<a href="#" class="link float-right">Forget Password ?</a>
-				</div>
-				<div class="form-action mb-3">
-					<a href="#" class="btn btn-primary btn-rounded btn-login">Sign In</a>
-				</div>
-				<div class="login-account">
-					<span class="msg">Don't have an account yet ?</span>
-					<a href="#" id="show-signup" class="link">Sign Up</a>
-				</div>
-			</div>
-		</div>
-
-		<div class="container container-signup animated fadeIn">
-			<h3 class="text-center">Sign Up</h3>
-			<div class="login-form">
-				<div class="form-group form-floating-label">
-					<input id="fullname" name="fullname" type="text" class="form-control input-border-bottom" required>
-					<label for="fullname" class="placeholder">Fullname</label>
-				</div>
-				<div class="form-group form-floating-label">
-					<input id="email" name="email" type="email" class="form-control input-border-bottom" required>
-					<label for="email" class="placeholder">Email</label>
-				</div>
-				<div class="form-group form-floating-label">
-					<input id="passwordsignin" name="passwordsignin" type="password" class="form-control input-border-bottom" required>
-					<label for="passwordsignin" class="placeholder">Password</label>
-					<div class="show-password">
-						<i class="icon-eye"></i>
+					<div class="row form-sub m-0">
+						<div class="custom-control custom-checkbox">
+							<input type="checkbox" class="custom-control-input" id="rememberme" name="rememberme">
+							<label class="custom-control-label" for="rememberme">Remember Me</label>
+						</div>
 					</div>
-				</div>
-				<div class="form-group form-floating-label">
-					<input id="confirmpassword" name="confirmpassword" type="password" class="form-control input-border-bottom" required>
-					<label for="confirmpassword" class="placeholder">Confirm Password</label>
-					<div class="show-password">
-						<i class="icon-eye"></i>
+					<div class="form-action mb-3">
+						<button type="submit" class="btn btn-primary btn-rounded btn-login"> Sign In</button>
 					</div>
-				</div>
-				<div class="row form-sub m-0">
-					<div class="custom-control custom-checkbox">
-						<input type="checkbox" class="custom-control-input" name="agree" id="agree">
-						<label class="custom-control-label" for="agree">I Agree the terms and conditions.</label>
-					</div>
-				</div>
-				<div class="form-action">
-					<a href="#" id="show-signin" class="btn btn-danger btn-link btn-login mr-3">Cancel</a>
-					<a href="#" class="btn btn-primary btn-rounded btn-login">Sign Up</a>
-				</div>
+				</form>
 			</div>
 		</div>
 	</div>
