ac7494e7454a07449b4c58f18e4346a8e4169a54
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-15 18:30:35 +0700

    Update Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,11 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/login', 'Auth::login');
+$routes->get('/register', 'Auth::register');
+$routes->get('/logout', 'Auth::logout');
+$routes->get('/admin', 'Admin::index');
+$routes->get('/users', 'Users::index');
 
 /*
  * --------------------------------------------------------------------
