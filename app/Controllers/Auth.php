79c045f404d1d8b4a195a78550362a6324034696
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-16 02:01:12 +0700

    Update Logingin Method at Auth

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -56,42 +56,64 @@ class Auth extends BaseController
         return view('auth/login', $data);
     }
 
-    public function register()
-    {
-        $data = [
-            'title' => 'Register'
-        ];
-
-        return view('auth/register', $data);
-    }
-
-    public function forgotPassword()
-    {
-        $data = [
-            'title' => 'Forgot Password'
-        ];
-
-        return view('auth/forgot-password', $data);
-    }
-
-    public function resetPassword()
-    {
-        $data = [
-            'title' => 'Reset Password'
-        ];
-
-        return view('auth/reset-password', $data);
-    }
-
     public function logingin()
     {
-    }
+        $nik = $this->request->getVar('nik');
+        $password = $this->request->getVar('password');
+        $rememberme = $this->request->getVar('rememberme');
 
-    public function registering()
-    {
+        $user = $this->usersModel->getUsers($nik);
+
+        // Check if user exists
+        if (!$user) {
+            session()->setFlashdata('message', 'NIK tidak terdaftar');
+            return redirect()->to('/auth/adminlogin');
+        }
+
+        // Check if password is correct
+        if (!password_verify($password, $user['password'])) {
+            session()->setFlashdata('message', 'Password salah');
+            return redirect()->to('/auth/adminlogin');
+        }
+
+        // Check if user role is admin or ketua RT
+        if ($user['role'] != 'admin' && $user['role'] != 'ketua_rt') {
+            session()->setFlashdata('message', 'Anda tidak memiliki akses');
+            return redirect()->to('/auth/adminlogin');
+        }
+
+        // Set session
+        $data = [
+            'nik' => $user['nik'],
+            'no_kk' => $user['no_kk'],
+            'nama' => $user['nama'],
+            'status' => $user['status'],
+            'jenis_kelamin' => $user['jenis_kelamin'],
+            'tempat_lahir' => $user['tempat_lahir'],
+            'tgl_lahir' => $user['tgl_lahir'],
+            'usia' => $user['usia'],
+            'status_perkawinan' => $user['status_perkawinan'],
+            'pendidikan' => $user['pendidikan'],
+            'email' => $user['email'],
+            'role' => $user['role'],
+            'foto' => $user['foto'],
+            'isLoggedIn' => true
+        ];
+
+        session()->set($data);
+
+        // Check if user want to remember me
+        if ($rememberme == 'on') {
+            // set Session expiry for 1 day
+            $this->session->sess_expiration = 86400;
+        }
+
+        return redirect()->to('/admin');
     }
 
     public function logout()
     {
+        session()->destroy();
+        return redirect()->to('/auth/adminlogin');
     }
 }
