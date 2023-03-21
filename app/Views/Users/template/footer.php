e15c4412e4222f23d5cca495b1b2405a6f03a30c
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-03-21 22:29:40 +0700

    Create User page template (footer)

---
+++ app/Views/Users/template/footer.php
@@ -0,0 +1,61 @@
+<!-- footer -->
+<footer class="big-footer">
+    <section class="container">
+        <div class="row footer-container-top">
+            <!-- left -->
+            <div class="col-md-4">
+                <div class="footer-top__left">
+                    <a href="./index.html" class="footer-logo-link">
+                        <figure class="footer-logo-wrapper">
+                            <img src="./assets/svg/warga-letter-logo.svg" alt="warga-letter-icon" />
+                        </figure>
+                    </a>
+                    <p class="text-basic">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. In
+                        assumenda.<br />
+                        Jl. Raya Kebayoran Lama Bl A-5/194, DKI Jakarta, Kota Jakarta
+                        12240
+                    </p>
+                    <button class="btn btn-main" type="button">
+                        Kontak Kami
+                    </button>
+                </div>
+            </div>
+            <!-- right -->
+            <div class="col-md-8">
+                <div class="footer-menu-link">
+                    <div class="footer-menu-wrapper">
+                        <ul class="ps-0 ps-md-5">
+                            <li class="footer-menu__header">Warga</li>
+                            <li><a href="./news.html">Pengumuman</a></li>
+                            <li><a href="./galery.html">Galeri</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-menu-wrapper">
+                        <ul class="ps-0 ps-md-5">
+                            <li class="footer-menu__header">Layanan</li>
+                            <li>
+                                <a href="./administration.html">Pengajuan Administrasi</a>
+                            </li>
+                            <li><a href="./report.html">Pelaporan</a></li>
+                            <li><a href="./family.html">Menu Keluarga</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <hr class="" />
+        <div class="footer-floor">
+            <p>
+                @<span id="yearNow"></span> Warga | Warga neque consequatur nemo
+                fugit voluptatem mollitia minima tempore.
+            </p>
+            <div class="footer-social-media">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+            </div>
+        </div>
+    </section>
+</footer>
+<!-- end of footer -->
