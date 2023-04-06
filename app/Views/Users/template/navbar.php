39e4d5579331df35e1bf29130e6f21a916c3493f
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-04-06 19:13:04 +0700

    Update Navbar

--- app/Views/Users/template/navbar.php
+++ app/Views/Users/template/navbar.php
@@ -12,27 +12,27 @@
             <!-- left -->
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item nav-items dropdown nav-item-dropdown">
-                    <a class="nav-link navbar-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link navbar-link dropdown-toggle  <?php if ($navbar == 'administrasi' || $navbar == 'pelaporan' || $navbar == 'keluarga') echo ' active'; ?>" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Warga
                     </a>
                     <ul class="dropdown-menu nav-dropdown-menu shadow">
                         <li>
-                            <a class="dropdown-item nav-dropdown-item" href="./administration.html"><span><i class="fa-solid fa-rocket"></i></span>Administrasi</a>
+                            <a class="dropdown-item nav-dropdown-item <?php if ($navbar == 'administrasi') echo ' active'; ?>" href="/users/administrasi"><span><i class="fa-solid fa-rocket"></i></span>Administrasi</a>
                         </li>
                         <li>
-                            <a class="dropdown-item nav-dropdown-item" href="./report.html"><span><i class="fa-solid fa-house-signal"></i></span>Pelaporan</a>
+                            <a class="dropdown-item nav-dropdown-item <?php if ($navbar == 'pelaporan') echo ' active'; ?>" href="/users/pelaporan"><span><i class="fa-solid fa-house-signal"></i></span>Pelaporan</a>
                         </li>
                         <li>
-                            <a class="dropdown-item nav-dropdown-item" href="./family.html"><span><i class="fa-solid fa-seedling"></i></span>Menu
+                            <a class="dropdown-item nav-dropdown-item <?php if ($navbar == 'keluarga') echo ' active'; ?>" href="/users/keluarga"><span><i class="fa-solid fa-seedling"></i></span>Menu
                                 keluarga</a>
                         </li>
                     </ul>
                 </li>
                 <li class="nav-item nav-items">
-                    <a class="nav-link navbar-link" href="./galery.html">Galeri</a>
+                    <a class="nav-link navbar-link <?php if ($navbar == 'galeri') echo ' active'; ?>" href="/users/galeri">Galeri</a>
                 </li>
                 <li class="nav-item nav-items">
-                    <a class="nav-link navbar-link" href="./news.html">Pengumuman</a>
+                    <a class="nav-link navbar-link <?php if ($navbar == 'pengumuman') echo ' active'; ?>" href="/users/pengumuman">Pengumuman</a>
                 </li>
             </ul>
             <!-- right -->
