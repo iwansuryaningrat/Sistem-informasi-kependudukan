a9d91d494d959ddcaf4c26eebecbb058d60335f0
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-04-06 19:13:12 +0700

    Update Layout

--- app/Views/Users/template/layout.php
+++ app/Views/Users/template/layout.php
@@ -57,6 +57,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/js/all.min.js" integrity="sha512-2bMhOkE/ACz21dJT8zBOMgMecNxx0d37NND803ExktKiKdSzdwn+L7i9fdccw/3V06gM/DBWKbYmQvKMdAA9Nw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <!-- custom -->
     <script src="/homepage/assets/js/scripts.js"></script>
+
+    <!-- Internal Script -->
+    <?= $this->renderSection('script'); ?>
 </body>
 
 </html>
