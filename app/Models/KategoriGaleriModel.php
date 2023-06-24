5206c76355026994d34d6198a7a5380b92733f2a
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-06-25 01:53:14 +0700

    Create New Model

---
+++ app/Models/KategoriGaleriModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class KategoriGaleriModel extends Model
+{
+    protected $table            = 'kategori_galeri';
+    protected $primaryKey       = 'kategori_galeri_id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $protectFields    = true;
+    protected $allowedFields    = ['nama_kategori', 'created_at', 'updated_at'];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+}
