1f9bcf6382f860d086b9db2a228846256c1f4407
iwansuryaningrat <iwan.suryaningrat28@gmail.com>
2023-08-07 21:12:56 +0700

    Create Initial Commit

---
+++ app/Database/Seeds/InitialSeeds.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class InitialSeeds extends Seeder
+{
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+    }
+
+    public function run()
+    {
+        $dataKeluarga = [
+            'no_kk' => 1212121212121212,
+            'nama_kepala_keluarga'    => "Admin",
+            'alamat' => '-',
+            'alamat_asal' => '-',
+            'foto_rumah' => 'default.png',
+            'tgl_pindah' => null,
+            'status' => 'Tetap',
+            'isExist' => true,
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ];
+        $this->db->table('keluarga')->insert($dataKeluarga);
+
+        $dataUser = [
+            'nik' => 22222222222222222,
+            'no_kk' => $dataKeluarga['no_kk'],
+            'nama' => "Admin",
+            'status' => 'Kepala Keluarga',
+            'jenis_kelamin' => 'Laki-laki',
+            'tempat_lahir' => '-',
+            'tgl_lahir' => date('Y-m-d H:i:s'),
+            'usia' => 0,
+            'status_perkawinan' => 'Kawin',
+            'pendidikan' => 'S3',
+            'email' => 'admin@gmail.com',
+            'password' => password_hash('admin1234', PASSWORD_DEFAULT),
+            'role' => 'Admin',
+            'foto' => 'default.png',
+            'status_kependudukan' => 'Tetap',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s'),
+        ];
+        $this->db->table('users')->insert($dataUser);
+    }
+}
