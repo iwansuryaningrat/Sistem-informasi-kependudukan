d4277f388ef858de8172865ee4ceb8b89254b2af
Iwan Suryaningrat <iwan.suryaningrat28@gmail.com>
2023-08-10 08:04:23 +0700

    Push

---
+++ app/Views/Admin/edit/editpenduduk.php
@@ -0,0 +1,200 @@
+<?= $this->extend('admin/template/layout'); ?>
+
+<?= $this->section('admin'); ?>
+
+<div class="container">
+    <div class="page-inner">
+        <div class="page-header">
+            <h4 class="page-title">Tambah Data Keluarga</h4>
+            <ul class="breadcrumbs">
+                <li class="nav-home">
+                    <a href="/admin">
+                        <i class="flaticon-home"></i>
+                    </a>
+                </li>
+                <li class="separator">
+                    <i class="flaticon-right-arrow"></i>
+                </li>
+                <li class="nav-item">
+                    <a href="/admin/families">Data Keluarga</a>
+                </li>
+                <li class="separator">
+                    <i class="flaticon-right-arrow"></i>
+                </li>
+                <li class="nav-item">
+                    <a href="/admin/addfamily">Tambah Data Keluarga</a>
+                </li>
+            </ul>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="card-title">Tambahkan Data Keluarga</div>
+                        <div class="card-category">Tambahkan data keluarga berdasarkan kartu keluarga</a></div>
+                    </div>
+
+                    <form id="exampleValidation" action="" method="POST" enctype="multipart/form-data">
+                        <div class="card-body">
+                            <div class="form-group form-show-validation row">
+                                <label for="no_kk" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Nomor Kartu Keluarga </label>
+                                <div class="col-lg-4 col-md-9 col-sm-8">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Masukkan Nomor Kartu Keluarga" aria-label="no_kk" aria-describedby="username-addon" id="no_kk" name="no_kk" value="<?= $keluarga['no_kk'] ?>" disabled>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group form-show-validation row">
+                                <label for="name" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Nama Kepala Keluarga</label>
+                                <div class="col-lg-4 col-md-9 col-sm-8">
+                                    <input type="text" class="form-control" id="name" name="name" placeholder="Masukkan Nama Kepala Keluarga" value="<?= $keluarga['nama_kepala_keluarga'] ?>" disabled>
+                                </div>
+                            </div>
+                            <div class="form-group form-show-validation row">
+                                <label for="alamat" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Alamat <span class="required-label">*</span></label>
+                                <div class="col-lg-4 col-md-9 col-sm-8">
+                                    <input type="text" class="form-control" id="alamat" placeholder="Masukkan Alamat" value="<?= $keluarga['alamat'] ?>" required>
+                                </div>
+                            </div>
+                            <div class="form-group form-show-validation row">
+                                <label for="alamat_asal" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Alamat Asal</label>
+                                <div class="col-lg-4 col-md-9 col-sm-8">
+                                    <input type="text" class="form-control" id="alamat_asal" placeholder="Masukkan Alamat Asal" value="<?= $keluarga['alamat_asal'] ?>">
+                                </div>
+                            </div>
+                            <div class="form-group form-show-validation row">
+                                <label for="tgl_pindah" class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Tanggal Pindah </span></label>
+                                <div class="col-lg-4 col-md-9 col-sm-8">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" id="tgl_pindah" name="tgl_pindah">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text">
+                                                <i class="fa fa-calendar-o"></i>
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="separator-solid"></div>
+                            <div class="form-group form-show-validation row">
+                                <label class="col-lg-3 col-md-3 col-sm-4 mt-sm-2 text-right">Foto Rumah</label>
+                                <div class="col-lg-4 col-md-9 col-sm-8">
+                                    <div class="input-file input-file-image">
+                                        <img class="img-upload-preview img-circle" width="100" height="100" src="http://placehold.it/100x100" alt="preview">
+                                        <input type="file" class="form-control form-control-file" id="fotoRumah" name="fotoRumah" accept="image/*">
+                                        <label for="fotoRumah" class="btn btn-primary btn-round btn-lg"><i class="fa fa-file-image"></i> Upload Foto</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-action">
+                            <div class="row">
+                                <div class="col-md-9"></div>
+                                <div class="col-md-3">
+                                    <button class="btn btn-danger">Cancel</button>
+                                    <input class="btn btn-success" type="submit" value="Submit">
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
+
+<?= $this->section('script'); ?>
+
+<script>
+    $('#tgl_pindah').datetimepicker({
+        format: 'MM/DD/YYYY'
+    });
+
+    $('#state').select2({
+        theme: "bootstrap"
+    });
+
+    /* validate */
+
+    // validation when select change
+    $("#state").change(function() {
+        $(this).valid();
+    })
+
+    $("#exampleValidation").validate({
+        validClass: "success",
+        rules: {
+            gender: {
+                required: true
+            },
+            confirmpassword: {
+                equalTo: "#password"
+            },
+            birth: {
+                date: true
+            },
+            fotoRumah: {
+                required: true,
+            },
+        },
+        highlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-error');
+        },
+        success: function(element) {
+            $(element).closest('.form-group').removeClass('has-error').addClass('has-success');
+        },
+    });
+
+    jQuery(document).ready(function() {
+        SweetAlert2Demo.init();
+    });
+</script>
+
+<script>
+    //== Class definition
+    var SweetAlert2Demo = function() {
+
+        //== Demos
+        var initDemos = function() {
+
+            <?php if (session()->getFlashdata('error')) : ?>
+                swal("Ups!", "<?= session()->getFlashdata('error') ?>", {
+                    icon: "error",
+                    buttons: {
+                        confirm: {
+                            className: 'btn btn-danger'
+                        }
+                    },
+                });
+            <?php endif; ?>
+
+            <?php if (session()->getFlashdata('success')) : ?>
+                swal("Selamat!", "<?= session()->getFlashdata('success') ?>", {
+                    icon: "success",
+                    buttons: {
+                        confirm: {
+                            className: 'btn btn-success'
+                        }
+                    },
+                });
+            <?php endif; ?>
+        };
+
+        return {
+            //== Init
+            init: function() {
+                initDemos();
+            },
+        };
+    }();
+
+    //== Class Initialization
+    jQuery(document).ready(function() {
+        SweetAlert2Demo.init();
+    });
+</script>
+
+<?= $this->endSection(); ?>
