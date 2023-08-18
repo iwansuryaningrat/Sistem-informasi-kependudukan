326c64a164c2d56cf0ff0ef49eda81688251bbd0
roziqinkhoeru <roziqinkhoeru8@gmail.com>
2023-08-18 15:40:57 +0700

    `update` empty state pengumuman

--- app/Views/Users/news/announcement.php
+++ app/Views/Users/news/announcement.php
@@ -20,23 +20,40 @@ use App\Helpers\DateHelper;
 <main>
   <section class="container container-space-top">
     <div class="news-container">
-      <?php foreach ($pengumuman as $data) : ?>
-        <a href="/users/detailpengumuman/<?= $data['pengumuman_id'] ?>" class="news-card">
-          <figure class="news-image__wrap" style="margin-bottom: 14px;">
-            <img src="/upload/photos/pengumuman/<?= $data['thumbnail'] ?>" alt="Pengumuman <?= $data['judul_pengumuman'] ?>" class="news-image__photo" />
-          </figure>
-          <p class="news-title-card mb-2"><?= $data['judul_pengumuman'] ?></p>
-          <p class="text-basic news-desc mb-2"><?= $data['deskripsi'] ?></p>
-          <div class="d-flex align-items-center">
-            <p class="mb-0">
-              <span class="me-2 text-gray"><i class="fa-solid fa-circle-user me-1 fill-gray"></i><?= $data['nama'] ?></span>
-              <span class="me-2 text-gray"><i class="fa-solid fa-clock me-1 fill-gray"></i><?= DateHelper::formatIndonesianDate($data['tanggal']) ?></span>
-              <span class="me-2 text-gray"><i class="fa-solid fa-location-dot me-1 fill-gray"></i><?= $data['tempat'] ?></span>
-            </p>
+      <?php if (empty($pengumuman)) : ?>
+        <div class="col-span-full text-center">
+          <div class="d-flex justify-content-center">
+            <div class="card-empty mt-0 mb-5">
+              <img src="/homepage/assets/img/decoration/out-of-stock.png" alt="empty pengumuman" class="img-empty-state mx-auto mb-1">
+              <p class="text-center mb-1 text-basic">
+                Maaf, tidak ada pengumuman yang tersedia saat ini.
+              </p>
+              <a class="btn btn-secondaries shadow" href="<?= $isLoggedin ? '/users' : '/home'; ?>">
+                Kembali ke Beranda
+              </a>
+            </div>
           </div>
-        </a>
-      <?php endforeach ?>
+        </div>
+      <?php else : ?>
+        <?php foreach ($pengumuman as $data) : ?>
+          <a href="/users/detailpengumuman/<?= $data['pengumuman_id'] ?>" class="news-card">
+            <figure class="news-image__wrap" style="margin-bottom: 14px;">
+              <img src="/upload/photos/pengumuman/<?= $data['thumbnail'] ?>" alt="Pengumuman <?= $data['judul_pengumuman'] ?>" class="news-image__photo" />
+            </figure>
+            <p class="news-title-card mb-2"><?= $data['judul_pengumuman'] ?></p>
+            <p class="text-basic news-desc mb-2"><?= $data['deskripsi'] ?></p>
+            <div class="d-flex align-items-center">
+              <p class="mb-0">
+                <span class="me-2 text-gray"><i class="fa-solid fa-circle-user me-1 fill-gray"></i><?= $data['nama'] ?></span>
+                <span class="me-2 text-gray"><i class="fa-solid fa-clock me-1 fill-gray"></i><?= DateHelper::formatIndonesianDate($data['tanggal']) ?></span>
+                <span class="me-2 text-gray"><i class="fa-solid fa-location-dot me-1 fill-gray"></i><?= $data['tempat'] ?></span>
+              </p>
+            </div>
+          </a>
+        <?php endforeach ?>
+      <?php endif; ?>
     </div>
+
   </section>
 </main>
 <!-- end of main -->
